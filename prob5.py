2b0c3b6c1bddba340d65bce5cca1e4244a0c581e
Colin Sturm <drcsturm@gmail.com>
2016-07-25 18:03:15 -0500

    solved prob5 and prob6

---
+++ prob5.py
@@ -0,0 +1,19 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+
+20:2,2,5
+19
+18:2,3,3
+17
+16:2,2,2,2
+15:3,5
+14:2,7
+13
+12:2,2,3
+11
+10:2,5
+
+2,2,2,2,3,3,5,7,11,13,17,19
---
+++ prob6.py
@@ -0,0 +1,16 @@
+# The sum of the squares of the first ten natural numbers is,
+
+# 1**2 + 2**2 + ... + 10**2 = 385
+# The square of the sum of the first ten natural numbers is,
+
+# (1 + 2 + ... + 10)**2 = 55**2 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+square_sum = 0
+num_sum = 0
+for i in range(101):
+    square_sum += i**2
+    num_sum += i
+print(num_sum**2 - square_sum)

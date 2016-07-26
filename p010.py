3e7bfc4571a81b736c7a9e543e41731a8e572165
Colin Sturm <drcsturm@gmail.com>
2016-07-26 12:56:53 -0500

    solved prob10, changed file names, updated prime detection

---
+++ p010.py
@@ -0,0 +1,8 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+# Find the sum of all the primes below two million.
+
+from utils.primes import primesfrom2to
+
+plist = primesfrom2to(2000000)
+print(sum(plist))

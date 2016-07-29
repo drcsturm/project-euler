89f8e9cd3f25786947cf3ef49c1bb306c51a8762
Colin Sturm <drcsturm@gmail.com>
2016-07-29 11:59:53 -0500

    start p017

---
+++ p017.py
@@ -0,0 +1,28 @@
+
+
+# If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
+w = {}
+w[1] = 3
+w[2] = 3
+w[3] = 5
+w[4] = 4
+w[5] = 4
+w[6] = 3
+w[7] = 5
+w[8] = 5
+w[9] = 4
+w[10] = 3
+w[20] = 6
+w[30] = 6
+w[40] = 5
+w[50] = 5
+w[60] = 5
+w[70] = 7
+w[80] = 6
+w[90] = 6
+w[100] = 10

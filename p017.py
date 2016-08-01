e4efd8148adbf8c6fe9e5d5e7c5dafe50d335ddb
Colin Sturm <drcsturm@gmail.com>
2016-08-01 09:04:12 -0500

    solved p017.py

--- p017.py
+++ p017.py
@@ -17,6 +17,15 @@ w[7] = 5
 w[8] = 5
 w[9] = 4
 w[10] = 3
+w[11] = 6
+w[12] = 6
+w[13] = 8
+w[14] = 8
+w[15] = 7
+w[16] = 7
+w[17] = 9
+w[18] = 8
+w[19] = 8
 w[20] = 6
 w[30] = 6
 w[40] = 5
@@ -25,4 +34,32 @@ w[60] = 5
 w[70] = 7
 w[80] = 6
 w[90] = 6
-w[100] = 10
+w[100] = w[1] + 10 # 10 = hundred and
+w[200] = w[2] + 10
+w[300] = w[3] + 10
+w[400] = w[4] + 10
+w[500] = w[5] + 10
+w[600] = w[6] + 10
+w[700] = w[7] + 10
+w[800] = w[8] + 10
+w[900] = w[9] + 10
+w[1000] = 11
+
+tens = 0
+for i in range(1,10):
+    tens += w[i]
+
+one_to_nineteen = tens
+for i in range(10,20):
+    one_to_nineteen += w[i]
+
+less_than_1hundred = one_to_nineteen
+for i in range(20,100,10):
+    less_than_1hundred += w[i]*10 + tens
+
+total = less_than_1hundred + w[1000]
+for i in range(100,1000,100):
+    total += w[i]*100 + less_than_1hundred - 3 # remove the first and of the hundred section, e.g. one hundred = 100 which does not contain and
+
+print(total)
+
